e120e3d0ae6b48143e33a2586ade01f900cdbc64
t1101675 <gu-yx17@mails.tsinghua.edu.cn>
2019-09-06 22:29:12 +0800

    back to dark

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -262,7 +262,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public void setDark() {
         Log.d(LOG_TAG, "set dark");
         WindowManager.LayoutParams p = getWindow().getAttributes();
-        p.screenBrightness = 0.3f;
+        p.screenBrightness = 0.1f;
 //        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         getWindow().setAttributes(p);
     }
